dfd2cce5bcbfb2ff2b601f76ff9bc1f20ff4e11b
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-22 17:27:56 +0530

    added theme

--- components/custom/Header.tsx
+++ components/custom/Header.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Container from "./Container";
-import { Moon, Sun } from "lucide-react";
+import { Menu, Moon, Sun } from "lucide-react";
+import ProfileButton from "./ProfileButton";
 
 const routes = [
   {
@@ -15,11 +19,33 @@ const routes = [
 ];
 
 function Header() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <header className='sm:flex sm:justify-between py-3 px-4 border-b'>
       <Container>
         <div className='relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between w-full'>
           <div className='flex items-center'>
+            <Sheet>
+              <SheetTrigger>
+                <Menu className='h-6 md:hidden w-6' />
+              </SheetTrigger>
+              <SheetContent side='left' className='w-[300px] sm:w-[400]'>
+                <nav className='flex flex-col gap-4'>
+                  {routes.map((route, i) => (
+                    // <Button key={i} asChild variant='ghost'>
+                    <Link
+                      key={i}
+                      href={route.href}
+                      className='block px-2 py-1 text-lg'
+                    >
+                      {route.label}
+                    </Link>
+                    // </Button>
+                  ))}
+                </nav>
+              </SheetContent>
+            </Sheet>
             <Link href='/' className='ml-4 lg:ml-0'>
               <h1 className='text-xl font-bold'>Transactions</h1>
             </Link>
@@ -42,11 +68,13 @@ function Header() {
               size='icon'
               className='mr-2'
               aria-label='Toggle Theme'
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             >
               <Sun className='h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
               <Moon className=' absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
               <span className='sr-only'>Toggle Theme</span>
             </Button>
+            <ProfileButton />
           </div>
         </div>
       </Container>
