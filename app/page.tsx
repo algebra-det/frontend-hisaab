122dd1a1552bb3bda219df5b31eb48396f65bb5b
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-22 21:30:17 +0530

    index message on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import getIndexData from "@/lib/home/getIndex";
 
-export default function Home() {
+interface indexType {
+  message: String;
+}
+export default async function Home() {
+  const indexCall: Promise<indexType> = getIndexData();
+  const indexData = await indexCall;
   return (
     <div className='grid place-content-center mt-4'>
       <h2>List your transactions</h2>
+      <p className='mt-4'>{indexData.message}</p>
     </div>
   );
 }
---
+++ lib/home/getIndex.ts
@@ -0,0 +1,15 @@
+const getIndexData = async () => {
+  console.log("Process: ", process.env.API_URL);
+  const res = await fetch(`${process.env.API_URL}`, {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  console.log("tran: ", res.ok, res.status);
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const jsonResponse = await res.json();
+  console.log("index Data: ", jsonResponse);
+  return jsonResponse;
+};
+export default getIndexData;
