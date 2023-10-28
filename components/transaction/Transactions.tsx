48ec0c9a72e14e87ce60c4cf2abd229d4807b589
Akash Chauhan <akash.th4kur@gmail.com>
2023-10-28 18:49:46 +0530

    updated card div height

--- components/transaction/Transactions.tsx
+++ components/transaction/Transactions.tsx
@@ -10,7 +10,7 @@ function Transactions({
   const transactions: Transaction[] = dummyTransactions;
   return (
     <>
-      <div className='overflow-auto sm:h-2/5 sm:w-96'>
+      <div className='overflow-auto w-80 sm:h-2/5 sm:w-96'>
         <div>
           {transactions.map((q) => (
             <div className='mt-2' key={q.id}>
