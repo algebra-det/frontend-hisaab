c0410d4b5db02b0d6258bce09ebeb996407de98e
Akash Chauhan <akash.th4kur@gmail.com>
2023-11-19 04:12:54 +0530

    Date Picker today default

--- components/transaction/DatePicker.tsx
+++ components/transaction/DatePicker.tsx
@@ -1,39 +1,47 @@
-"use client";
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+'use client'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover";
-import dayjs from "dayjs";
-import { CalendarDays } from "lucide-react";
-import { useEffect, useState } from "react";
-import { dateFormatAPI, dateFormatDisplay } from "@/config/format";
+} from '@/components/ui/popover'
+import dayjs from 'dayjs'
+import { CalendarDays } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { dateFormatAPI, dateFormatDisplay } from '@/config/format'
+import { SelectSingleEventHandler } from 'react-day-picker'
 
 export function DatePicker({
   changeDate,
 }: {
-  changeDate: (value: string) => void;
+  changeDate: (value: string) => void
 }) {
-  const today = new Date();
-  const [date, setDate] = useState<Date>();
-  const [open, setOpen] = useState(false);
+  const today = new Date()
+  const [date, setDate] = useState<Date>(today)
+  const [open, setOpen] = useState(false)
+
+  const updateSelectedDate: SelectSingleEventHandler = value => {
+    console.log('Selected is: ', value, typeof value, value?.toDateString())
+    const dateString: string | number | Date =
+      value?.toDateString() ?? new Date()
+    setDate(new Date(dateString))
+  }
 
   useEffect(() => {
-    changeDate(dayjs(date).format(dateFormatAPI));
-    setOpen(false);
-  }, [date]);
+    changeDate(dayjs(date).format(dateFormatAPI))
+    setOpen(false)
+  }, [date])
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
-          variant={"outline"}
+          variant={'outline'}
           className={cn(
-            "w-4/6 justify-start text-left font-normal ml-1",
-            !date && "text-muted-foreground"
+            'w-4/6 justify-start text-left font-normal ml-1',
+            !date && 'text-muted-foreground'
           )}
         >
           <CalendarDays className='mr-2 h-4 w-4' />
@@ -48,11 +56,11 @@ export function DatePicker({
         <Calendar
           mode='single'
           selected={date}
-          onSelect={setDate}
+          onSelect={updateSelectedDate}
           initialFocus
         />
       </PopoverContent>
     </Popover>
-  );
+  )
 }
-export default DatePicker;
+export default DatePicker
