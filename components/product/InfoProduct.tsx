c17f882fa2301009eb61fdbc5672bf1d4f94e92a
Akash Chauhan <akash.th4kur@gmail.com>
2023-11-19 18:41:10 +0530

    Info product details added

--- components/product/InfoProduct.tsx
+++ components/product/InfoProduct.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { getCookie } from 'cookies-next'
 import {
   Dialog,
   DialogContent,
@@ -7,7 +8,7 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog'
 import { Product } from '@/types'
-import { Dispatch, SetStateAction } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 function InfoProduct({
   product,
@@ -18,16 +19,72 @@ function InfoProduct({
   open: boolean
   setOpen: Dispatch<SetStateAction<boolean>>
 }) {
+  const [fetching, setFetching] = useState(false)
+  const [additionalInfo, setAdditionalInfo] = useState({} as Product)
+
+  const fetchProductDetails = async () => {
+    try {
+      setFetching(true)
+      let auth = getCookie('authorization')
+      if (!auth) auth = ''
+      console.log('Cookie: ', auth, typeof auth)
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/products/${product.id}`,
+        {
+          headers: {
+            Authorization: JSON.parse(JSON.stringify(auth)),
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+      const productAdditionalInfo = await response.json()
+      if (response.ok) {
+        setAdditionalInfo(productAdditionalInfo.data)
+      }
+    } catch (error) {
+    } finally {
+      setFetching(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchProductDetails()
+  }, [])
+
   return (
     <div>
       <Dialog open={open} onOpenChange={setOpen}>
         <DialogContent className='max-w-[325px] sm:max-w-[425px] md:max-w-[625px] lg:max-w-[625px]'>
           <DialogHeader>
             <DialogTitle>{product.productName}</DialogTitle>
-            <DialogDescription>
-              Add below details to create a new product.
-            </DialogDescription>
+            <DialogDescription>Product Description:</DialogDescription>
           </DialogHeader>
+          <div>
+            <div className='flex justify-between'>
+              <p className='text-sm'>Total Sales: </p>
+              <span>{product.totalSale} units</span>
+            </div>
+            <div className='flex justify-between'>
+              <p className='text-sm'>Purchase Price:</p>
+              <span>&#8377; {product.purchasePrice}</span>
+            </div>
+            <div className='flex justify-between'>
+              <p className='text-sm'>Last Selling Price:</p>
+              <span>&#8377; {product.lastSellingPrice}</span>
+            </div>
+          </div>
+
+          <DialogDescription>Additional Info:</DialogDescription>
+          {fetching ? (
+            <div className='animate-spin-slow ease-linear rounded-full border-dashed border-8 border-t-8 border-gray-200 h-6 w-6 sm:h-24 sm:w-24'></div>
+          ) : (
+            <div>
+              <div className='flex justify-between'>
+                <p className='text-sm'>Total Profit generated: </p>
+                <span>&#8377; {additionalInfo.totalProfit}</span>
+              </div>
+            </div>
+          )}
         </DialogContent>
       </Dialog>
     </div>
